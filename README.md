32f9cf0d2efe666c37ab9e424cb0d84e7bc53451
Mayank Abhishek <mail.mabhishek@gmail.com>
2019-05-08 20:11:55 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Reading List App
+Learning GraphQL Apollo with React 
+
+Running the app will create a book list keeping app as shown in below screenshot:
+
+![Final App Screenshot](https://i.imgur.com/OD1tcNx.png)
---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+// components
+import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "https://book-list-manager-server.mayankabhishek.now.sh/graphql"
+});
+
+class App extends Component {
+  render() {
+    return (
+      <ApolloProvider client={client}>
+        <div id="main">
+          <h1>Ninja's Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
+    );
+  }
+}
+
+export default App;
